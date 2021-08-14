e88cd64e2307bc204a0a4399ea10f41ea023065c
sanju6890 <sanjusaikap2001@gmail.com>
2021-08-14 19:55:11 +0530

    Added Project Files
    Signed-off-by: sanju6890 <sanjusaikap2001@gmail.com>

---
+++ app.py
@@ -0,0 +1,168 @@
+from tkinter import *
+from tkinter import messagebox
+
+import requests
+import json
+
+root = Tk()
+root.title("SK News App")
+root.iconbitmap("app_icon.ico")
+root.geometry("1080x720")
+root.minsize(1080,720)
+root.maxsize(1080,720)
+root.configure(bg='tan1')
+
+
+def india_news():
+    list_box.delete(0,END)
+    url="http://newsapi.org/v2/top-headlines?country=in&apiKey=3e33ba1948eb4504b47af11904512730"
+    # creating url request
+    news = requests.get(url) 
+    # creating python object from json
+    news_dict = json.loads(news.text)
+    contents = news_dict['articles']
+    list_box.insert(END,"INDIA NEWS...")
+    i = 1
+    for content in contents:
+        headline = f"{i}. {content['title']}"
+        list_box.insert(END,headline)
+        # discription = content['description']
+        # list_box.insert(END,discription)
+        i = i+1
+
+def health_news():
+    list_box.delete(0,END)
+    url = "http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=3e33ba1948eb4504b47af11904512730"
+    # creating url request
+    news = requests.get(url) 
+    # creating python object from json
+    news_dict = json.loads(news.text)
+    contents = news_dict['articles']
+    list_box.insert(END,"HEALTH NEWS...")
+    i = 1
+    for content in contents:
+        headline = f"{i}. {content['title']}"
+        list_box.insert(END,headline)
+        # discription = content['description']
+        # list_box.insert(END,discription)
+        i = i+1
+
+def science_news():
+    list_box.delete(0,END)
+    url="http://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=3e33ba1948eb4504b47af11904512730"
+    # creating url request
+    news = requests.get(url) 
+    # creating python object from json
+    news_dict = json.loads(news.text)
+    contents = news_dict['articles']
+    list_box.insert(END,"SCIENCE NEWS...")
+    i = 1
+    for content in contents:
+        headline = f"{i}. {content['title']}"
+        list_box.insert(END,headline)
+        # discription = content['description']
+        # list_box.insert(END,discription)
+        i = i+1
+
+def tech_news():
+    list_box.delete(0,END)
+    url="http://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=3e33ba1948eb4504b47af11904512730"
+    # creating url request
+    news = requests.get(url) 
+    # creating python object from json
+    news_dict = json.loads(news.text)
+    contents = news_dict['articles']
+    list_box.insert(END,"TECHNOLOGY NEWS...")
+    i = 1
+    for content in contents:
+        headline = f"{i}. {content['title']}"
+        list_box.insert(END,headline)
+        # discription = content['description']
+        # list_box.insert(END,discription)
+        i = i+1
+
+def enter_news():
+    list_box.delete(0,END)
+    url="http://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=3e33ba1948eb4504b47af11904512730"
+    # creating url request
+    news = requests.get(url) 
+    # creating python object from json
+    news_dict = json.loads(news.text)
+    contents = news_dict['articles']
+    list_box.insert(END,"ENTERTAINMENT NEWS...")
+    i = 1
+    for content in contents:
+        headline = f"{i}. {content['title']}"
+        list_box.insert(END,headline)
+        # discription = content['description']
+        # list_box.insert(END,discription)
+        i = i+1
+
+def sports_news():
+    list_box.delete(0,END)
+    url="http://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=3e33ba1948eb4504b47af11904512730"
+    # creating url request
+    news = requests.get(url) 
+    # creating python object from json
+    news_dict = json.loads(news.text)
+    contents = news_dict['articles']
+    list_box.insert(END,"SPORTS NEWS...")
+    i = 1
+    for content in contents:
+        headline = f"{i}. {content['title']}"
+        list_box.insert(END,headline)
+        # discription = content['description']
+        # list_box.insert(END,discription)
+        i = i+1
+
+def business_news():
+    list_box.delete(0,END)
+    url="http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=3e33ba1948eb4504b47af11904512730"
+    # creating url request
+    news = requests.get(url) 
+    # creating python object from json
+    news_dict = json.loads(news.text)
+    contents = news_dict['articles']
+    list_box.insert(END,"BUSINESS NEWS...")
+    i = 1
+    for content in contents:
+        headline = f"{i}. {content['title']}"
+        list_box.insert(END,headline)
+        # discription = content['description']
+        # list_box.insert(END,discription)
+        i = i+1
+
+# App's Header
+header = Label(root, text='Get The Latest News Updates',bg="black",fg="white",font=("Times", "16", "bold italic"),borderwidth=5, relief=SUNKEN)
+header.pack(pady=10)
+
+# List Box to display the news content
+list_box= Listbox(root, bg="black", fg="green2", font=("Times", "14", "italic"), relief = SUNKEN, borderwidth=10, height = 20 ,width = 110)
+list_box.pack(pady=5)
+
+# Control frame
+frame=Frame(root, borderwidth = 5, bg = 'black', relief = SUNKEN, height=20, width=30)
+frame.pack(pady=20)
+
+india = Button(frame, text='India',bg="purple1",fg="white",font=("Times", "14", "bold"),borderwidth=5,command=india_news)
+health = Button(frame, text='Health',bg="purple1",fg="white",font=("Times", "14", "bold"),borderwidth=5,command=health_news)
+science = Button(frame, text='Science',bg="purple1",fg="white",font=("Times", "14", "bold"),borderwidth=5,command=science_news)
+technology = Button(frame, text='Technology',bg="purple1",fg="white",font=("Times", "14", "bold"),borderwidth=5,command=tech_news)
+entertainment = Button(frame, text='Entertainment',bg="purple1",fg="white",font=("Times", "14", "bold"),borderwidth=5,command=enter_news)
+business = Button(frame, text='Business',bg="purple1",fg="white",font=("Times", "14", "bold"),borderwidth=5,command=business_news)
+sports = Button(frame, text='Sports',bg="purple1",fg="white",font=("Times", "14", "bold"),borderwidth=5,command=sports_news)
+
+# Position all the buttons in the control frame
+india.grid(row=0, column=0, padx=10)
+health.grid(row=0, column=1, padx=10)
+science.grid(row=0, column=2, padx=10)
+technology.grid(row=0, column=3, padx=10)
+entertainment.grid(row=0, column=4, padx=10)
+business.grid(row=0, column=5, padx=10)
+sports.grid(row=0, column=6, padx=10)
+
+# Copy Right Label
+label = Label(root, text='SANJAY KUMAR (C) 2021',font=("Times", "12", "bold"),borderwidth=5)
+label.pack(pady=10)
+
+root.mainloop()
